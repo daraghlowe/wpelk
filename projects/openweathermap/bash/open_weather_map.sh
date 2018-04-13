f36ce01ff2b7695bb60e9bf0b9913dabcea2910e
daraghlowe <daragh.lowe@wpengine.com>
2018-04-13 10:52:44 +0100

    changes to open_weather_map

--- projects/openweathermap/bash/open_weather_map.sh
+++ projects/openweathermap/bash/open_weather_map.sh
@@ -3,3 +3,14 @@
 
 #Pull our api key from secrets file
 API_KEY=$(cat secrets)
+CITY_LIST_FILE="city.list.json"
+
+function get_city_id_from_json(){
+  CITY_NAME=$1
+  COUNTRY_CODE=$2
+
+  CITY_ID=$(jq --arg CITY_NAME "${CITY_NAME}" --arg COUNTRY_CODE "${COUNTRY_CODE}" '.[] \
+  | select(.name | match($CITY_NAME;"i")) \
+  | select(.country | match($COUNTRY_CODE;"i")) \
+  | .id' $CITY_LIST_FILE)
+}
